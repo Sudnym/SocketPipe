df2e5e249933c8659e22d3fcd8d30efed633612f
Sudnym <mathescole@gmail.com>
2020-11-14 15:35:17 -0500

    Success?

--- SocketPipe.go
+++ SocketPipe.go
@@ -1,42 +1,36 @@
 package SocketPipe
 
 import (
-	"fmt"
-	"io/ioutil"
+	"bufio"
+	"io"
 	"net"
-	"sync"
 )
 
-func check(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
 func Pipe(sockOne net.Conn, sockTwo net.Conn) {
-	fmt.Println(sockOne.RemoteAddr())
-	fmt.Println(sockTwo.RemoteAddr())
 	go handleSocks(sockOne, sockTwo)
 	handleSocks(sockTwo, sockOne)
 }
 func handleSocks(sockOne net.Conn, sockTwo net.Conn) {
 	var buffer []byte
-	var wg sync.WaitGroup
 	ch := make(chan []byte)
 	for true {
-		wg.Add(1)
-		go read(&wg, sockOne, buffer, ch)
-		wg.Wait()
-		buffer = <-ch
-		sockTwo.Write(buffer)
+		buffer, isClosed := read(sockOne, buffer, ch)
+		if isClosed {
+			return
+		}
+		_, err := sockTwo.Write(buffer)
+		if err == io.EOF {
+			return
+		}
 	}
 }
-func read(wg *sync.WaitGroup, sock net.Conn, buffer []byte, ch chan []byte) {
-	defer wg.Done()
-	var buff []byte
-	for string(buff) == string(buffer) {
-		buffer, _ = ioutil.ReadAll(sock)
+func read(sock net.Conn, buffer []byte, ch chan []byte) ([]byte, bool) {
+	reader := bufio.NewReader(sock)
+	isClosed := false
+	var one []byte
+	buffer, _ = reader.ReadBytes('\n')
+	if _, err := sock.Read(one); err == io.EOF {
+		isClosed = true
 	}
-	fmt.Println(buffer)
-	ch <- buffer
+	return buffer, isClosed
 }
