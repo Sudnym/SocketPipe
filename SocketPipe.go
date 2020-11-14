362a6e7d05c16a045091d057d2a5d8981179deb9
Sudnym <mathescole@gmail.com>
2020-11-14 13:59:48 -0500

    huge change

--- SocketPipe.go
+++ SocketPipe.go
@@ -5,17 +5,29 @@ import (
 	"time"
 )
 
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func Pipe(sockOne net.Conn, sockTwo net.Conn) {
-	sockOne.SetDeadline(time.Now().Add(1))
-	sockTwo.SetDeadline(time.Now().Add(1))
-	channelTo := make(chan []byte)
-	channelFrom := make(chan []byte)
-	var bufferTo []byte
-	var bufferFrom []byte
-	go sockTwo.Read(bufferTo)
-	go func() { channelTo <- bufferTo }()
-	go sockOne.Write(<-channelTo)
-	go sockOne.Read(bufferFrom)
-	go func() { channelFrom <- bufferFrom }()
-	go sockTwo.Write(<-channelTo)
+	for {
+		go handleSocks(sockOne, sockTwo)
+		go handleSocks(sockTwo, sockOne)
+	}
+}
+func handleSocks(sockOne net.Conn, sockTwo net.Conn) {
+	var buffer []byte
+	err := sockOne.SetReadDeadline(time.Now().Add(5))
+	check(err)
+	err = sockTwo.SetWriteDeadline(time.Now().Add(5))
+	check(err)
+	num, err := sockOne.Read(buffer)
+	check(err)
+	if num > 0 {
+		_, err := sockTwo.Write(buffer)
+		check(err)
+	}
+
 }
