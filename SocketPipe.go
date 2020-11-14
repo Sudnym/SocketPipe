d31b90e6bfb9f7fb2ee5ad9afd036eaa2148eab4
Sudnym <mathescole@gmail.com>
2020-11-14 15:41:20 -0500

    \t instead of \n

--- SocketPipe.go
+++ SocketPipe.go
@@ -16,18 +16,20 @@ func handleSocks(sockOne net.Conn, sockTwo net.Conn) {
 	for true {
 		buffer, isClosed := read(sockOne, buffer, ch)
 		if isClosed {
+			sockTwo.Write([]byte("Other Client Closed\t"))
 			return
 		}
 		_, err := sockTwo.Write(buffer)
 		if err == io.EOF {
 			return
+			sockOne.Write([]byte("Other Client Closed\t"))
 		}
 	}
 }
 func read(sock net.Conn, buffer []byte, ch chan []byte) ([]byte, bool) {
 	reader := bufio.NewReader(sock)
 	isClosed := false
-	buffer, err := reader.ReadBytes('\n')
+	buffer, err := reader.ReadBytes('\t')
 	if err == io.EOF {
 		isClosed = true
 	}
