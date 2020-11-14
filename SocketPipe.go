c1a89d8908dfffe8a569de8c0dbc794f6d28abd4
Sudnym <mathescole@gmail.com>
2020-11-14 14:04:10 -0500

    removed error checking for useless errors

--- SocketPipe.go
+++ SocketPipe.go
@@ -19,15 +19,8 @@ func Pipe(sockOne net.Conn, sockTwo net.Conn) {
 }
 func handleSocks(sockOne net.Conn, sockTwo net.Conn) {
 	var buffer []byte
-	err := sockOne.SetReadDeadline(time.Now().Add(5))
-	check(err)
-	err = sockTwo.SetWriteDeadline(time.Now().Add(5))
-	check(err)
-	num, err := sockOne.Read(buffer)
-	check(err)
-	if num > 0 {
-		_, err := sockTwo.Write(buffer)
-		check(err)
-	}
-
+	_ = sockOne.SetReadDeadline(time.Now().Add(5))
+	_ = sockTwo.SetWriteDeadline(time.Now().Add(5))
+	_, _ = sockOne.Read(buffer)
+	_, _ = sockTwo.Write(buffer)
 }
