f0c3d16db5821453fd5919a64c68c5645a0c616c
Sudnym <mathescole@gmail.com>
2020-11-14 13:40:13 -0500

    truncated

--- SocketPipe.go
+++ SocketPipe.go
@@ -5,17 +5,17 @@ import (
 	"time"
 )
 
-func Pipe(sockOne net.Conn, sockTwo net.Conn){
-	sockOne.SetDeadline(time.Now().Add(0.1))
-	sockTwo.SetDeadline(time.Now().Add(0.1))
+func Pipe(sockOne net.Conn, sockTwo net.Conn) {
+	sockOne.SetDeadline(time.Now().Add(1))
+	sockTwo.SetDeadline(time.Now().Add(1))
 	channelTo := make(chan []byte)
 	channelFrom := make(chan []byte)
 	var bufferTo []byte
 	var bufferFrom []byte
 	go sockTwo.Read(bufferTo)
 	go func() { channelTo <- bufferTo }()
-	go sockOne.Write(<- channelTo)
+	go sockOne.Write(<-channelTo)
 	go sockOne.Read(bufferFrom)
 	go func() { channelFrom <- bufferFrom }()
-	go sockTwo.Write(<- channelTo)
+	go sockTwo.Write(<-channelTo)
 }
